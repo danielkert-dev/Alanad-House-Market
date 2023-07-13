28ed935978ab61b44cb71cf512b4a313e3793613
Daniel Kert <danielkertdev@gmail.com>
2023-07-13 18:44:23 +0300

    23 jun 13 18:44

--- project/house_maklar/views.py
+++ project/house_maklar/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import MaklerHouse
 
 # Create your views here.
@@ -6,4 +7,9 @@ from .models import MaklerHouse
 def maklar(request):
     houses = MaklerHouse.objects.all()
 
-    return render(request, 'house_maklar/maklar.html' , {'houses': houses})
+    
+    paginator = Paginator(houses, 10)  # Set the number of houses per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'house_maklar/maklar.html' , {'page_obj': page_obj})
